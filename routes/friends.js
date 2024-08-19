2d795a47e5fd7e41ed4f1b94b78a57d3fdb70004
Sumukha Rao H <sumukharaoh05@gmail.com>
2024-08-19 06:53:40 +0530

    removed_accept_req

--- routes/friends.js
+++ routes/friends.js
@@ -11,7 +11,7 @@ router.get('/requests', isLoggedIn, async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-
+        res.render('acceptfriends', { user: req.user, friendRequests: user.friendRequests });
         res.json({ friendRequests: user.friendRequests });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -42,41 +42,6 @@ router.post('/send-request', isLoggedIn, async (req, res) => {
         await recipient.save();
 
         res.status(200).json({ message: 'Friend request sent' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-router.post('/accept-request', isLoggedIn, async (req, res) => {
-    try {
-        const { senderId } = req.body;
-
-        if (!senderId) {
-            return res.status(400).json({ message: 'Sender ID is required' });
-        }
-
-        const recipient = await User.findById(req.user._id);
-        const sender = await User.findById(senderId);
-
-        if (!recipient || !sender) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        if (!recipient.friendRequests.includes(senderId)) {
-            return res.status(400).json({ message: 'No friend request from this user' });
-        }
-
-        // Add each other as friends
-        recipient.friends.push(sender._id);
-        sender.friends.push(recipient._id);
-
-        // Remove friend request
-        recipient.friendRequests = recipient.friendRequests.filter(id => !id.equals(senderId));
-
-        await recipient.save();
-        await sender.save();
-
-        res.status(200).json({ message: 'Friend request accepted' });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
